45a7e6393933ee23674e09d7903076c087aadda2
ImJustLucas <lucas.bellier@icloud.com>
2024-05-17 14:15:58 +0200

    ✨ add requester package

---
+++ packages/requester/src/request/request.ts
@@ -0,0 +1,30 @@
+import { APIError, APIResponse } from "@kascad/shared-types";
+import axios, { type AxiosRequestConfig } from "axios";
+
+type ApiRequestConfig = Exclude<AxiosRequestConfig, "method">;
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_PATH,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  timeout: 6000,
+  transformRequest: [(data) => JSON.stringify(data)],
+});
+
+const request = async <TData>(
+  url: string,
+  options?: ApiRequestConfig
+): Promise<APIResponse<TData>> => {
+  const response = instance<APIResponse<TData>>(url, { ...options })
+    .then((response) => response.data)
+    .catch((error: APIError) => {
+      throw error;
+    });
+
+  return response;
+};
+
+export type { ApiRequestConfig };
+export default request;
