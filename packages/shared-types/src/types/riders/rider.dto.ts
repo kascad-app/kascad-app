8a88c42a33325adee286dfb56104a623d42977c8
ImJustLucas <lucas.bellier@icloud.com>
2025-04-04 16:27:50 +0200

    refactor: Clean up rider and sponsor DTOs by removing unused type properties

--- packages/shared-types/src/types/riders/rider.dto.ts
+++ packages/shared-types/src/types/riders/rider.dto.ts
@@ -2,17 +2,15 @@ import { ProfileType } from "../profiles";
 import { GenderIdentity } from "..";
 
 export type registerRiderDto = {
-	email: string;
-	password: string;
-	birthDate: Date;
-	firstName: string;
-	lastName: string;
-	type: ProfileType.RIDER;
-	gender: GenderIdentity;
+  email: string;
+  password: string;
+  birthDate: Date;
+  firstName: string;
+  lastName: string;
+  gender: GenderIdentity;
 };
 
 export type loginRiderDto = {
-	email: string;
-	password: string;
-	type: ProfileType.RIDER;
+  email: string;
+  password: string;
 };
--- packages/shared-types/src/types/sponsors/sponsor.dto.ts
+++ packages/shared-types/src/types/sponsors/sponsor.dto.ts
@@ -1,14 +1,12 @@
 import { ProfileType } from "../profiles";
 
 export type registerSponsorDto = {
-	email: string;
-	password: string;
-	type: ProfileType.SPONSOR;
-	companyName: string;
+  email: string;
+  password: string;
+  companyName: string;
 };
 
 export type loginSponsorDto = {
-	email: string;
-	password: string;
-	type: ProfileType.SPONSOR;
+  email: string;
+  password: string;
 };
